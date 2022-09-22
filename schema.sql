da9f554c4edda03d3aa9c60b7486944a9a0f7fb0
ijeoma <ijeomaodinko@gmail.com>
2022-09-22 00:56:33 -0400

    add files.

--- schema.sql
+++ schema.sql
@@ -2,17 +2,3 @@
 
 /*to create a database with the name vet_clinic*/
 
-CREATE DATABASE vet_clinic;
-
-/* to create a table with name animals in vet_clinic database*/
-
-CREATE TABLE animals (
-    id BIGSERIAL PRIMARY KEY,
-    name varchar(100) NOT NULL,
-    date_of_birth Date NOT NULL,
-    escape_attempts INT NOT NULL,
-    neutered BOOLEAN NOT NULL,
-    weight_kg DECIMAL NOT NULL
-);
-/* to add another column species*/
-ALTER TABLE animals ADD COLUMN species VARCHAR (100); 
